9cfa50342f57e3dfdcaf19b1d860009d5ab49170
shahadat742380 <shahadat.peacockindia@gmail.com>
2024-03-14 16:32:04 +0600

    added clerk theme

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,13 @@
 import { SignUp } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
 
 export default function Page() {
   return (
     <div className="flex justify-center py-24">
-      <SignUp />
+      <SignUp
+      appearance={{
+        baseTheme: dark,
+      }} />
     </div>
   );
 }
